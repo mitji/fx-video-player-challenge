103b8c260ae6aa05529ecbafb173ba77d59a8234
Giorgio Mazzei <giorgio@fxdigital.uk>
2022-09-16 15:23:15 +0200

    feat: add forward/rewind

--- index.js
+++ index.js
@@ -5,48 +5,60 @@ let video;
 let player;
 
 async function initPlayer() {
-    try {
-        await loadShaka();
-    
-        let manifestUri;
-    
-        manifestUri = 'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd';
-        shakaInitPlayer(manifestUri);
-    } catch (e) {
-        alert(e);
-    }
+  try {
+    await loadShaka();
+
+    let manifestUri;
+
+    manifestUri =
+      "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
+    shakaInitPlayer(manifestUri);
+  } catch (e) {
+    alert(e);
+  }
 }
 
 // set up basic functionality
 function loadPlayer() {
-    if (!player) {
-        initPlayer();
-    } else {
-        alert("Player already loaded");
-    }
+  if (!player) {
+    initPlayer();
+  } else {
+    alert("Player already loaded");
+  }
 }
 
 function pausePlay() {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function moveTime(time) {
+  if (video) {
+    let newTime = video.currentTime + time;
+
+    if (newTime > video.duration) {
+      newTime = video.duration;
+    } else if (newTime < 0) {
+      newTime = 0;
     }
+
+    video.currentTime = newTime;
+  }
 }
 
 function destroyPlayer() {
-    player.destroy();
-    player = null;
+  player.destroy();
+  player = null;
 
-    // reset subtitles
-    document
-        .querySelector(".video-container__subtitles").innerText = "";
-    document
-        .querySelector(".video-container__subtitle-tracks")
-        .classList.add("hide");
-    document
-        .querySelector(".video-container__subtitle-tracks")
-        .innerHTML = "";
+  // reset subtitles
+  document.querySelector(".video-container__subtitles").innerText = "";
+  document
+    .querySelector(".video-container__subtitle-tracks")
+    .classList.add("hide");
+  document.querySelector(".video-container__subtitle-tracks").innerHTML = "";
 }
 
 // set up handlers
@@ -57,21 +69,21 @@ const ffBtn = document.querySelector(".ff-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 
 loadBtn.addEventListener("click", () => {
-    loadPlayer();
+  loadPlayer();
 });
 
 playPauseBtn.addEventListener("click", () => {
-    pausePlay();
+  pausePlay();
 });
 
 destroyBtn.addEventListener("click", () => {
-    destroyPlayer();
+  destroyPlayer();
 });
 
 rewindBtn.addEventListener("click", () => {
-
+  moveTime(-5);
 });
 
 ffBtn.addEventListener("click", () => {
-    
+  moveTime(5);
 });
